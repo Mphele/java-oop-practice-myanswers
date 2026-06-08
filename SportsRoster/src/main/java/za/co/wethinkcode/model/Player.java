b691d8085da782e7d117f93b1a6c56d78173e277
mphele <mphelemoswane3@gmail.com>
2026-06-08 21:36:24 +0200

    Player class implemented

--- SportsRoster/src/main/java/za/co/wethinkcode/model/Player.java
+++ SportsRoster/src/main/java/za/co/wethinkcode/model/Player.java
@@ -1,4 +1,48 @@
 package za.co.wethinkcode.model;
 
+import java.util.Objects;
+
 public class Player {
+
+    private int jerseyNUmber;
+    private String name;
+    private boolean isInjured;
+
+    public Player(int jerseyNumber, String name, boolean isInjured){
+        if(name ==null || name.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+
+        if (jerseyNumber<1){
+            throw new IllegalArgumentException();
+        }
+
+        this.jerseyNUmber = jerseyNumber;
+        this.name=name;
+        this.isInjured = isInjured;
+    }
+
+    public int getJerseyNUmber() {
+        return jerseyNUmber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isInjured() {
+        return isInjured;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return jerseyNUmber == player.jerseyNUmber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(jerseyNUmber);
+    }
 }
