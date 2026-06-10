728db8f65c0d298e7005c37ff9eeeb701f4a2702
mphele <mphelemoswane3@gmail.com>
2026-06-10 20:22:13 +0200

    Library Item class implemented

--- LIbraryManagementSysten/src/main/java/za/co/wethinkcode/model/LibraryItem.java
+++ LIbraryManagementSysten/src/main/java/za/co/wethinkcode/model/LibraryItem.java
@@ -1,4 +1,30 @@
 package za.co.wethinkcode.model;
 
-public class LibraryItem {
+public abstract class LibraryItem {
+
+    private String itemId;
+    private String title;
+    protected ItemStatus status;
+
+    public LibraryItem(String itemId, String title){
+        if(itemId==null || title == null || itemId.isEmpty() || title.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+
+        this.itemId = itemId;
+        this.title = title;
+        this.status = ItemStatus.AVAILABLE;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ItemStatus getStatus() {
+        return status;
+    }
 }
