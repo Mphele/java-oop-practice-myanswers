9816b6be16379246b575a80c028be06d03751e57
mphele <mphelemoswane3@gmail.com>
2026-06-10 17:54:07 +0200

    Booking and Chargeable implemented

--- HotelBillingSystem/src/main/java/za/co/wethinkcode/model/RoomBooking.java
+++ HotelBillingSystem/src/main/java/za/co/wethinkcode/model/RoomBooking.java
@@ -1,4 +1,50 @@
 package za.co.wethinkcode.model;
 
-public class RoomBooking {
+import za.co.wethinkcode.service.Chargeable;
+
+public class RoomBooking implements Chargeable {
+
+    private String roomNumber;
+    private int nights;
+    private double baseRate;
+
+    public RoomBooking(String roomNumber, int nights, double baseRate){
+        if(roomNumber==null||roomNumber.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        this.roomNumber = roomNumber;
+
+        if(nights<1){
+            throw new IllegalArgumentException();
+        }
+        this.nights = nights;
+
+        if (baseRate<0.0){
+            throw new IllegalArgumentException();
+        }
+        this.baseRate = baseRate;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+
+    public double getBaseRate() {
+        return baseRate;
+    }
+
+
+    @Override
+    public double calculateCharge() {
+        return nights*baseRate;
+    }
+
+    @Override
+    public String getChargeDescription() {
+        return String.format("Room %s for %d nights", roomNumber, nights);
+    }
 }
--- HotelBillingSystem/src/main/java/za/co/wethinkcode/service/Chargeable.java
+++ HotelBillingSystem/src/main/java/za/co/wethinkcode/service/Chargeable.java
@@ -1,4 +1,8 @@
 package za.co.wethinkcode.service;
 
 public interface Chargeable {
+
+    public double calculateCharge();
+    public String getChargeDescription();
+
 }
