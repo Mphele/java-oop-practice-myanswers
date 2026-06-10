45630bbbdd25f5f194bc0a03200a46290b56a381
mphele <mphelemoswane3@gmail.com>
2026-06-10 20:23:19 +0200

    Book class implemented

--- LIbraryManagementSysten/src/main/java/za/co/wethinkcode/model/Book.java
+++ LIbraryManagementSysten/src/main/java/za/co/wethinkcode/model/Book.java
@@ -1,4 +1,28 @@
 package za.co.wethinkcode.model;
 
-public class Book {
+import za.co.wethinkcode.service.Borrowable;
+
+public class Book extends LibraryItem implements Borrowable {
+
+    public Book(String itemId, String title){
+        super(itemId, title);
+    }
+
+    @Override
+    public void checkout() {
+        if(getStatus()!=ItemStatus.AVAILABLE){
+            throw new IllegalStateException();
+        }
+        status= ItemStatus.CHECKED_OUT;
+    }
+
+    @Override
+    public void returnItem() {
+
+        if(getStatus()!=ItemStatus.CHECKED_OUT){
+            throw new IllegalStateException();
+        }
+        status = ItemStatus.AVAILABLE;
+
+    }
 }
