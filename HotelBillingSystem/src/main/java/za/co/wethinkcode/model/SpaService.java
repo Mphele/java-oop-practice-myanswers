141bc76b12789e662696ae7fb63d8ca3c7f53af1
mphele <mphelemoswane3@gmail.com>
2026-06-10 18:02:13 +0200

    Spa Service implemented

--- HotelBillingSystem/src/main/java/za/co/wethinkcode/model/SpaService.java
+++ HotelBillingSystem/src/main/java/za/co/wethinkcode/model/SpaService.java
@@ -1,4 +1,37 @@
 package za.co.wethinkcode.model;
 
-public class SpaService {
+import za.co.wethinkcode.service.Chargeable;
+
+public class SpaService implements Chargeable {
+
+    private String serviceName;
+    private boolean isPremium;
+
+    public SpaService(String serviceName, boolean isPremium){
+        if (serviceName==null||serviceName.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        this.serviceName=serviceName;
+        this.isPremium = isPremium;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+
+    @Override
+    public double calculateCharge() {
+        return isPremium ? 150.0:75 ;
+    }
+
+    @Override
+    public String getChargeDescription() {
+        String type = isPremium ? "Premium":"Standard";
+        return String.format("%s Spa Service: %s", type, serviceName);
+    }
 }
