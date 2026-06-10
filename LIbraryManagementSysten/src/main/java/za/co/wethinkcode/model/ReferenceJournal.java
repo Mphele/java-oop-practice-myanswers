47522e342a93b620ae1418790ee20bb43648fda9
mphele <mphelemoswane3@gmail.com>
2026-06-10 20:27:52 +0200

    ReferenceJournal class implemented

--- LIbraryManagementSysten/src/main/java/za/co/wethinkcode/model/ReferenceJournal.java
+++ LIbraryManagementSysten/src/main/java/za/co/wethinkcode/model/ReferenceJournal.java
@@ -1,4 +1,21 @@
 package za.co.wethinkcode.model;
 
-public class ReferenceJournal {
+public class ReferenceJournal extends LibraryItem{
+
+    private int floorNumber;
+
+    public ReferenceJournal(String itemId, String title, int floorNumber){
+        super(itemId, title);
+        if(floorNumber<1){
+            throw new IllegalArgumentException();
+        }
+        this.floorNumber = floorNumber;
+    }
+
+    public int getFloorNumber(){
+        return floorNumber;
+    }
+
+
+
 }
